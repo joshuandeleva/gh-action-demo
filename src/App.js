38be9e867a373d99665ed871bf711dfd21764178
joshuandeleva <joshuandeleva90@gmail.com>
2022-07-09 13:13:16 +0300

    changes test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo192.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +15,7 @@ function App() {
         >
           Learn React
         </a>
+        <h1>Hello welcom to githun actions</h1>
       </header>
     </div>
   );
